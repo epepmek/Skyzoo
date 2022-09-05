bc84cf1c5b818cfbf5e3ea33cb2b572384edd0d6
Rezaofc8 <109743116+Rezaofc8@users.noreply.github.com>
2022-09-05 14:47:55 +0700

    Update owner-donasi.js

--- plugins/owner-donasi.js
+++ plugins/owner-donasi.js
@@ -5,15 +5,15 @@ let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 
 *PAYMENT ↓*
-_*Pulsa/pulse(Telkomsel):*_ ${pulsa}
-
-_*Dana/ovo:*_ ${dana}
-_*Paypal:*_ ${paypal}
-_*Saweria:*_ ${saweria}
-_*Trakteer:*_ ${trakteer}
+_*Pulsa(3 [tri]):*_ ${pulsa}
+_*Dana:*_ ${dana}
+*DONASI DENGAN FOLLOW IG OWNER JUGA BOLEH*
+https://instagram.com/itz.reza_official_
 Setelah melakukan donasi kirim bukti pembayaran ke owner
 `
-await conn.send2ButtonDoc(m.chat, str, author, '🔖 Matikan Fitur', '.off detect', 'ℹ️ Menu', '.menu', fpayment, adReply)
+conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/Rezaofc', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+['𝙼𝙴𝙽𝚄', '/menu']
+], m)
 
 }
 handler.help = ['donasi']
